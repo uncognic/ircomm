5cf11ad13957e49ea9151c0c96e385f1c8f578d3
uncognic <uncognic@proton.me>
2026-01-24 20:46:24 -0500

    move about to a xaml file

---
+++ ircomm/Views/AboutWindow.xaml.cs
@@ -0,0 +1,18 @@
+using System.Windows;
+
+namespace ircomm
+{
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
